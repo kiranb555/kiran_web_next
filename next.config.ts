965cc891ef3e8ae0a1feda56b1d06955fe38d3a2
kiranb555 <kiran.basavaraj05@gmail.com>
2025-10-08 00:21:33 +0530

    renamed out file

--- next.config.ts
+++ next.config.ts
@@ -4,23 +4,9 @@ import path from 'path';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  distDir: 'dist',
+  distDir: 'out',
   images: {
     unoptimized: true
-  },
-  webpack: (config) => {
-    // Ensure aliases from tsconfig are used in webpack
-    if (!config.resolve) {
-      config.resolve = {};
-    }
-    if (!config.resolve.alias) {
-      config.resolve.alias = {};
-    }
-    
-    // Add path aliases from tsconfig
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-    
-    return config;
   }
 };
 
