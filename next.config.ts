89d1a6509470618c5869afe44ffb81d2a84d5662
kiranb555 <kiran.basavaraj05@gmail.com>
2025-10-22 22:32:45 +0530

    image not loading fix

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'export',
   distDir: 'out',
-  // Ensure correct paths when deploying to GitHub Pages
   basePath: '/kiran_web_next',
-  assetPrefix: '/kiran_web_next',
   trailingSlash: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
   images: {
-    unoptimized: true, // Required for static exports
+    unoptimized: true,
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'randomuser.me',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
+      { protocol: 'https', hostname: 'randomuser.me' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
     ],
   },
 };
-
-export default nextConfig;
